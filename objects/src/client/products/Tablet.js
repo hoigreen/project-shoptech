8d68301f620db4f816dfbb1fb78b6d2aedf05471
Truong Quoc Hoi <88929110+hoigreen@users.noreply.github.com>
2023-03-23 08:21:26 +0700

    upload code 23/03

--- objects/src/client/products/Tablet.js
+++ objects/src/client/products/Tablet.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import Nav from '../common/Nav'
+import Footer from '../common/Footer'
+
 const Tablet = ({ socket }) => {
 
     const navigate = useNavigate();
 
     return (
         <div>
-            
+            <Nav socket={socket} />
+            <div className="container">
+                <div className="grid wide">
+111
+                </div>
+            </div>
+            <Footer socket={socket} />
         </div>
     );
 
